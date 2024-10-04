3d09a8169d611b240e663661d697a7103d37a5c1
spikeyamk <spikeyamk@github.com>
2024-10-04 19:33:33 +0200

    UART_HWCONTROL_NONE fix

--- src/main.c
+++ src/main.c
@@ -206,7 +206,7 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
   huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   if (HAL_UART_Init(&huart1) != HAL_OK)
   {
