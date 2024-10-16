f3a8e7a1c3973b80213b34338202b26117c0b1d2
spikeyamk <spikeyamk@github.com>
2024-10-16 12:20:35 +0200

    printf redirected to usart3 also

--- src/core/util/printf_redirect.c
+++ src/core/util/printf_redirect.c
@@ -4,8 +4,10 @@
 int __io_putchar(int ch) {
     if (ch == '\n') {
         HAL_UART_Transmit(&huart1, (const uint8_t*) "\r\n", 2, 1000);  // Send both at once
+        HAL_UART_Transmit(&huart3, (const uint8_t*) "\r\n", 2, 1000);  // Send both at once
     } else {
         HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 1000);  // Send character
+        HAL_UART_Transmit(&huart3, (uint8_t *)&ch, 1, 1000);  // Send character
     }
     return ch;
 }
