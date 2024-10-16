57cebaf1c39843a87141f687faba4a5d3183c023
spikeyamk <spikeyamk@github.com>
2024-10-16 10:48:26 +0200

    spi3 max31865 broken

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -19,35 +19,104 @@
 #include "gpio.h"
 #include "spi.h"
 
-void select() {
-    HAL_GPIO_WritePin(SPI2_NSS_GPIO_Port, SPI2_NSS_Pin, GPIO_PIN_RESET);
+namespace max6549 {
+    void select() {
+        HAL_GPIO_WritePin(SPI2_NSS_GPIO_Port, SPI2_NSS_Pin, GPIO_PIN_RESET);
+    }
+
+    void deselect() {
+        HAL_GPIO_WritePin(SPI2_NSS_GPIO_Port, SPI2_NSS_Pin, GPIO_PIN_SET);
+    }
+
+    auto write(const uint8_t address, const uint8_t value) {
+        select();
+        const std::array<uint8_t, 2> buf { ((address) & 0x7E), value };
+        const auto ret { HAL_SPI_Transmit(&hspi2, buf.data(), buf.size(), 1000) };
+        deselect();
+        return ret;
+    }
+
+    uint8_t read(const uint8_t address) {
+        select();
+        const uint8_t buf { ((address) | 0x80) };
+        HAL_SPI_Transmit(&hspi2, &buf, sizeof(buf), 1000);
+        uint8_t ret { 0x00 };
+        HAL_SPI_Receive(&hspi2, &ret, sizeof(ret), 1000);
+        deselect();
+        return ret;
+    }
+
+    void test() {
+        std::printf("write(0x20, 0xFF): 0x%02X\n", write(0x20, (0xEF << 1)));
+        std::printf("read(0x20): 0x%02X\n", read(0x20));
+    }
 }
 
-void deselect() {
-    HAL_GPIO_WritePin(SPI2_NSS_GPIO_Port, SPI2_NSS_Pin, GPIO_PIN_SET);
-}
+namespace max31865 {
+    namespace select {
+        void a() {
+            HAL_GPIO_WritePin(SPI3_NSS1_GPIO_Port, SPI3_NSS0_Pin, GPIO_PIN_RESET);
+        }
 
-auto write(const uint8_t address, const uint8_t value) {
-    select();
-    const std::array<uint8_t, 2> buf { ((address) & 0x7E), value };
-    const auto ret { HAL_SPI_Transmit(&hspi2, buf.data(), buf.size(), 1000) };
-    deselect();
-    return ret;
-}
+        void b() {
+            HAL_GPIO_WritePin(SPI3_NSS0_GPIO_Port, SPI3_NSS1_Pin, GPIO_PIN_RESET);
+        }
+    }
 
-uint8_t read(const uint8_t address) {
-    select();
-    const uint8_t buf { ((address) | 0x80) };
-    HAL_SPI_Transmit(&hspi2, &buf, sizeof(buf), 1000);
-    uint8_t ret { 0x00 };
-    HAL_SPI_Receive(&hspi2, &ret, sizeof(ret), 1000);
-    deselect();
-    return ret;
-}
+    namespace deselect {
+        void a() {
+            HAL_GPIO_WritePin(SPI3_NSS0_GPIO_Port, SPI3_NSS0_Pin, GPIO_PIN_SET);
+        }
 
-void test_max6549() {
-    std::printf("write(0x20, 0xFF): 0x%02X\n", write(0x20, (0xEF << 1)));
-    std::printf("read(0x20): 0x%02X\n", read(0x20));
+        void b() {
+            HAL_GPIO_WritePin(SPI3_NSS0_GPIO_Port, SPI3_NSS1_Pin, GPIO_PIN_SET);
+        }
+    }
+
+    namespace write {
+        auto a(const uint8_t address, const uint8_t value) {
+            select::a();
+            const std::array<uint8_t, 2> buf { (address | 0x80), value };
+            const auto ret { HAL_SPI_Transmit(&hspi3, buf.data(), buf.size(), 500) };
+            deselect::a();
+            return ret;
+        }
+
+        auto b(const uint8_t address, const uint8_t value) {
+            select::b();
+            const std::array<uint8_t, 2> buf { (address | 0x80), value };
+            const auto ret { HAL_SPI_Transmit(&hspi3, buf.data(), buf.size(), 500) };
+            deselect::b();
+            return ret;
+        }
+    }
+
+    namespace read {
+        uint8_t a(const uint8_t address) {
+            select::a();
+            const uint8_t buf { (address & 0x7E) };
+            HAL_SPI_Transmit(&hspi3, &buf, sizeof(buf), 500);
+            uint8_t ret { 0x00 };
+            HAL_SPI_Receive(&hspi3, &ret, sizeof(ret), 500);
+            deselect::a();
+            return ret;
+        }
+
+        uint8_t b(const uint8_t address) {
+            select::b();
+            const uint8_t buf { (address & 0x7E) };
+            HAL_SPI_Transmit(&hspi3, &buf, sizeof(buf), 500);
+            uint8_t ret { 0x00 };
+            HAL_SPI_Receive(&hspi3, &ret, sizeof(ret), 500);
+            deselect::b();
+            return ret;
+        }
+    }
+
+    void test() {
+        std::printf("write::a(0x00, 0x00): 0x%02X\n", write::a(0x00, 0x80));
+        std::printf("read::a(0x00): 0x%02X\n", read::a(0x00));
+    }
 }
 
 /// This function calculates the area of a rectangle.
@@ -72,7 +141,8 @@ void app_main(void* arg) {
 
     //actu::bridge::a::cool();
     //actu::bridge::b::cool();
-    Trielo::trielo<test_max6549>();
+    Trielo::trielo<max6549::test>();
+    Trielo::trielo<max31865::test>();
     for(
         uint16_t dac_value = 0;
         true;
