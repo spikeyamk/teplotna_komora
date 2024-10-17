fa78d73141fa336db5221379ddd8d083ea7b3626
spikeyamk <spikeyamk@github.com>
2024-10-17 17:30:21 +0200

    actu::fan::test_speed added

--- src/actu/fan/include/actu/fan/fan.hpp
+++ src/actu/fan/include/actu/fan/fan.hpp
@@ -14,6 +14,8 @@ namespace fan {
      * Stops all 6 available fans. Additionally sends stop signal to all 8 fan control
      * pins (2 fans not available at the moment).
     */
+    void set_speed(const uint8_t speed_0_to_100);
+    void test_speed();
     void stop_all();
     void start_all();
     void start_all_half_speed();
