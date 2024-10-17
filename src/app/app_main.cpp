4479bf2588e571beda6199d2c660112548254166
spikeyamk <spikeyamk@github.com>
2024-10-17 15:15:39 +0200

    app restruct cleanup producer_consumer_test

--- src/app/app_main.cpp
+++ src/app/app_main.cpp
@@ -20,6 +20,7 @@
 #include "spi.h"
 #include "MAX31865.hpp"
 #include "cmsis_os2.h"
+#include "producer_consumer_test.hpp"
 #include "app/app_main.hpp"
 
 namespace max6549 {
@@ -410,37 +411,6 @@ namespace max31865 {
             read::b(0x00)
         );
     }
-}
-
-void produce(void* arg) {
-    uint32_t* product { reinterpret_cast<uint32_t*>(arg) };
-    while(1) {
-        std::printf("produce: product: %lu\n", (*product)--);
-        osDelay(500);
-    }
-}
-
-void consume(void* arg) {
-    uint32_t* product { reinterpret_cast<uint32_t*>(arg) };
-    while(1) {
-        std::printf("consume: product: %lu\n", (*product)++);
-        osDelay(150);
-    }
-}
-
-void launch_producer_consumer_test(uint32_t& product) {
-    const osThreadAttr_t producer_attr {
-        .name = "producer",
-        .stack_size = 4 * 1024,
-        .priority = (osPriority_t) osPriorityNormal,
-    };
-    osThreadNew(produce, &product, &producer_attr);
-    const osThreadAttr_t consumer_attr {
-        .name = "consumer",
-        .stack_size = 4 * 1024,
-        .priority = (osPriority_t) osPriorityNormal,
-    };
-    osThreadNew(consume, &product, &consumer_attr);
 }
 
 /// This function calculates the area of a rectangle.
---
+++ src/app/producer_consumer_test.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cstdint>
+
+void produce(void* arg);
+void consume(void* arg);
+void launch_producer_consumer_test(uint32_t& product);
