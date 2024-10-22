a2f9c8aac3820e7ec4d2cfa9656438d1ba3d1924
spikeyamk <spikeyamk@github.com>
2024-10-22 23:19:56 +0200

    added boost::sml2

--- src/comm/rs232_uart/rs232_uart.cpp
+++ src/comm/rs232_uart/rs232_uart.cpp
@@ -1,6 +1,7 @@
+#include <sml>
 #include <trielo/trielo.hpp>
-#include <serde/serde.hpp>
-#include <boost/sml.hpp>
+#include <serde/serializer.hpp>
+#include <serde/deserializer.hpp>
 #include "comm/rs232_uart/rs232_uart.hpp"
 
 namespace comm {
