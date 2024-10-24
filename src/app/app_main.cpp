9653fd92d0cea0c20a981d2cf970225254195811
Lexbalker <lexbalkerplays@gmail.com>
2024-10-24 10:49:17 +0200

    change to encoder_test()

--- src/app/app_main.cpp
+++ src/app/app_main.cpp
@@ -33,12 +33,12 @@ void app_main(void* arg) {
 
     actu::fan::start_min_speed();
 
-    actu::lin_source::test_dac();
+    //actu::lin_source::test_dac();
     
-    /*for (;;) {
+    for (;;) {
         panel::sevseg::white::display_refresh(12345);
         panel::encoder::encoder_test();
-    }*/
+    }
 
     /*for(uint32_t tick = 0; true; tick++) {
         std::printf("app_main: tick: %lu\n", tick);
