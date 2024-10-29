a36bf81c795690aa6bf79ba71d1727832a8d04b2
Lexbalker <lexbalkerplays@gmail.com>
2024-10-29 16:30:02 +0100

    removed useless code

--- src/app/app_main.cpp
+++ src/app/app_main.cpp
@@ -41,7 +41,8 @@ extern "C" void app_main(void* arg) {
     Trielo::trielo<example_subdirectory::foo>();
     Trielo::trielo<util::turn_every_annoying_peripheral_off>();
 
-    float counter = -999.0f;
+    //float counter = -999.0f;
+    float counter = 0.0f;
     panel::sevseg::white::launch_display_task(counter);
 
     for(uint32_t tick = 0; true; tick++) {
--- src/panel/encoder/include/panel/encoder/encoder.hpp
+++ src/panel/encoder/include/panel/encoder/encoder.hpp
@@ -1,99 +1,12 @@
 #pragma once
 
 #include <iostream>
-#include <bitset>
-#include <array>
 #include "main.h"
 #include "stm32f2xx_hal.h"
 
 namespace panel {
 namespace encoder {
-    /*void encoder_test() {
-        int counter = 0;
-        
-        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10) == GPIO_PIN_RESET) {
-            if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11) == GPIO_PIN_RESET) {
-
-                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11) == GPIO_PIN_RESET) {};
-                std::printf("Counter: %d\n", counter++);
-                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10) == GPIO_PIN_RESET) {};
-                HAL_Delay(10);
-            }
-
-            if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11) == GPIO_PIN_SET) {
-
-                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11) == GPIO_PIN_SET) {};
-                std::printf("Counter: %d\n", counter--);
-                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10) == GPIO_PIN_RESET) {};
-                while (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11) == GPIO_PIN_RESET) {};
-                HAL_Delay(10);
-            }
-
-            if (counter < 0) counter = 0;
-            if (counter > 16) counter = 16;
-    }
-    }*/
-
-    enum class Direction {
-        AntiClockWise,
-        ClockWise,
-    };
-
-    using State = std::bitset<2>;
-
-    struct Transition {
-        State before;
-        State after;
-        Direction direction;
-    };
-
-    constexpr std::array<Transition, 8> transitions {{
-        {
-            .before = { 00 },
-            .after = { 01 },
-            .direction = Direction::AntiClockWise
-        },
-        {
-            .before = { 00 },
-            .after = { 10 },
-            .direction = Direction::ClockWise
-        },
-        {
-            .before = { 01 },
-            .after = { 00 },
-            .direction = Direction::ClockWise
-        },
-        {
-            .before = { 01 },
-            .after = { 11 },
-            .direction = Direction::AntiClockWise
-        },
-        {
-            .before = { 10 },
-            .after = { 00 },
-            .direction = Direction::AntiClockWise
-        },
-        {
-            .before = { 10 },
-            .after = { 11 },
-            .direction = Direction::ClockWise
-        },
-        {
-            .before = { 11 },
-            .after = { 01 },
-            .direction = Direction::ClockWise
-        },
-        {
-            .before = { 11 },
-            .after = { 10 },
-            .direction = Direction::AntiClockWise
-        },
-    }};
-
-    extern State previous_state;
-
     inline void test() {
-        
         std::printf(
             "HAL_GPIO_ReadPin(ENCA_EXTI10_GPIO_Port, ENCA_EXTI10_Pin): 0x%02X\n",
             HAL_GPIO_ReadPin(ENCA_EXTI10_GPIO_Port, ENCA_EXTI10_Pin)
