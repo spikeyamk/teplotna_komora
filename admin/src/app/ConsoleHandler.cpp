99b73b998483c579ac5a28526e356c2b1752393b
Michael Murithi <mykemitrichev@gmail.com>
2024-11-02 23:47:20 +0100

    feat: console application initial implementation

--- admin/src/app/ConsoleHandler.cpp
+++ admin/src/app/ConsoleHandler.cpp
@@ -2,52 +2,206 @@
 #include <iostream>
 #include <fstream>
 #include "SerialHandler.hpp"
+#include <ctime>
+#include <sstream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 const std::string ConsoleHandler::sensorReadingsDirectoryPath = "sensor_readings/";
 const std::string ConsoleHandler::usageFilePath = "usage.txt";
 const std::string ConsoleHandler::logsFilePath = "logs.txt";
 const std::string ConsoleHandler::outputDirectoryPath = "output/";
+bool ConsoleHandler::exitApp = false;
 
 void ConsoleHandler::Run() {
-    return;
+    PrintMessage("Welcome to temperature regulator admin!\n");
+    PrintUsage();
+
+    CreateFileIfNotExists(logsFilePath);
+    CreateFileIfNotExists(usageFilePath);;
+
+    while (exitApp == false)
+    {
+        std::string command;
+        std::cout << "Enter a command: ";
+        std::cin >> command;
+
+        if (command == "read_all") {
+            ReadAll();
+        } else if (command == "set_temperature") {
+            float temperature;
+            std::cout << "Enter the temperature: ";
+            std::cin >> temperature;
+            SetTemperature(temperature);
+        } else if (command == "export_output") {
+            std::string destinationDirectoryPath;
+            std::cout << "Enter the destination directory path: ";
+            std::cin >> destinationDirectoryPath;
+            ExportOutput(destinationDirectoryPath);
+        } else if (command == "exit") {
+            Exit();
+        } else {
+            PrintError("Error: Invalid command. Please try again.");
+            PrintUsage();
+        }
+    }
 }
 
-void ConsoleHandler::ConfigureOutputDirectory(std::string outputDirectoryPath) {
-    return;
+void ConsoleHandler::ExportOutput(std::string destinationDirectoryPath) {
+    try {
+            // Check if the source directory exists
+            if (!fs::exists(destinationDirectoryPath) || !fs::is_directory(outputDirectoryPath)) {
+                return PrintError("Error: Source directory \"" + destinationDirectoryPath + "\" does not exist or is not a directory.");
+            }
+
+            // Create the destination directory if it does not exist
+            if (!fs::exists(destinationDirectoryPath)) {
+                fs::create_directories(destinationDirectoryPath);
+            }
+
+            // Iterate over each file in the source directory
+            for (const auto& entry : fs::directory_iterator(outputDirectoryPath)) {
+                if (entry.is_regular_file()) { // Check if the entry is a regular file
+                    fs::path sourceFile = entry.path();
+                    fs::path destinationFile = fs::path(outputDirectoryPath) / sourceFile.filename();
+
+                    // Copy the file from source to destination
+                    fs::copy_file(sourceFile, destinationFile, fs::copy_options::overwrite_existing);
+                    PrintSuccess("Copied: " + sourceFile.string() + " to " + destinationFile.string());
+                }
+            }
+        } catch (const fs::filesystem_error& e) {
+            PrintError("Filesystem error: " + std::string(e.what()));
+        } catch (const std::exception& e) {
+            PrintError("Error: " + std::string(e.what()));
+        }
 }
 
-void ConsoleHandler::ReadAll(std::string outputFileName) {
-    return;
+void ConsoleHandler::ReadAll() {
+    PrintMessage("Reading all sensor states\n");
+    std::string outputFileName = GenerateOutputFilePath();
+    PrintSuccess("All sensor states read succesfully!\n");
+    RecordAppLogs("All sensor states read and recorded at " + outputFileName + ".\n");
 }
 
 void ConsoleHandler::SetTemperature(float temperature) {
-    return;
+    PrintMessage("Setting temperature to " + std::to_string(temperature) + " degrees Celsius...\n");
+    PrintSuccess("Temperature has been set successfully!\n");
+    RecordAppLogs("Temperature set to " + std::to_string(temperature) + " degrees Celsius.\n");
 }
 
 void ConsoleHandler::Exit() {
-    return;
+    PrintMessage("Exiting the application...\n");
+    exitApp = true;
+    PrintSuccess("Application has been closed successfully!\n");
 }
 
-std::string ConsoleHandler::GenerateOutputFileName() {
-    return "";
+std::string ConsoleHandler::GenerateOutputFilePath() {
+    // Get the current time
+    std::time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
+
+    // Format the date as YYYY_MM_DD
+    std::ostringstream oss;
+    oss << outputDirectoryPath
+        << sensorReadingsDirectoryPath
+        << (now->tm_year + 1900) << "_"
+        << (now->tm_mon + 1) << "_"
+        << now->tm_mday
+        << "_Sensor_Readings.json";
+
+    return oss.str();
 }
 
-void ConsoleHandler::WriteData(std::string filename) {
-    return;
+void ConsoleHandler::CreateFileIfNotExists(std::string filePath) {
+    // Check if the file exists
+    std::ifstream fileCheck(filePath);
+    if (fileCheck) {
+        PrintMessage("File \"" + filePath + "\" already exists.");
+        return fileCheck.close();
+    }
+
+    // File does not exist, so we create it
+    std::ofstream fileCreate(filePath);
+    if (!fileCreate) {
+        return PrintError("Error: Unable to create file \"" + filePath + "\".");
+    }
+
+    PrintSuccess("File \"" + filePath + "\" has been created successfully.");
+    return fileCreate.close();
+}
+
+std::string GenerateLogLine(const std::string& message) {
+        // Get the current time
+        std::time_t t = std::time(nullptr);
+        std::tm* now = std::localtime(&t);
+
+        // Format the date and time as YYYY_MM_DD_HH:MM:SS
+        std::ostringstream oss;
+        oss << (now->tm_year + 1900) << "_"
+            << (now->tm_mon + 1) << "_"
+            << now->tm_mday << "_"
+            << (now->tm_hour < 10 ? "0" : "") << now->tm_hour << ":"
+            << (now->tm_min < 10 ? "0" : "") << now->tm_min << ":"
+            << (now->tm_sec < 10 ? "0" : "") << now->tm_sec
+            << " " << message;
+
+        return oss.str();
+}
+
+void ConsoleHandler::RecordAppLogs(const std::string& data) {
+    // Open the file in append mode (std::ios::app)
+    std::ofstream file(logsFilePath, std::ios::app);
+    if (!file) {
+        return PrintError("Error: Unable to record the application logs at " + logsFilePath);
+    }
+
+    // Write the data to the file
+    file << GenerateLogLine(data) << std::endl;
+
+    // Close the file
+    file.close();
+}
+
+void ConsoleHandler::WriteData(std::string filename, const std::string& data) {
+    std::ofstream file(filename, std::ios::app);
+    if (!file) {
+        return PrintError("Error: Unable to open file " + filename + " for writing.");
+    }
+
+    // Write the data to the file
+    file << data;
+
+    // Close the file
+    file.close();
+    PrintSuccess("Data written successfully to " + filename);
 }
 
 void ConsoleHandler::PrintMessage(const std::string& message) {
-    return;
+    std::cout << message << std::endl;
 }
 
 void ConsoleHandler::PrintError(const std::string& message) {
-    return;
+    std::cout << "\033[31m" << message << "\033[0m" << std::endl;
 }
 
 void ConsoleHandler::PrintSuccess(const std::string& message) {
-    return;
+    std::cout << "\033[32m" << message << "\033[0m" << std::endl;
 }
 
 void ConsoleHandler::PrintUsage() {
-    return;
+    std::ifstream file(usageFilePath); // Open the file for reading
+
+    if (!file) {
+        PrintError("Error: Unable to open file " + usageFilePath);
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) { // Read file line by line
+        PrintMessage(line);       // Print each line using printNormalMessage
+    }
+
+    file.close(); // Close the file
 }
--- admin/src/app/ConsoleHandler.hpp
+++ admin/src/app/ConsoleHandler.hpp
@@ -15,13 +15,16 @@ public:
     ~ConsoleHandler() = delete;
 
     void                        Run();
-    void                        ConfigureOutputDirectory(std::string outputDirectoryPath);
-    void                        ReadAll(std::string outputFileName);
+    void                        ExportOutput(std::string outputDirectoryPath);
+    void                        ReadAll();
     void                        SetTemperature(float temperature);
     void                        Exit();
 private:
-    std::string                 GenerateOutputFileName();
-    void                        WriteData(std::string filename);
+    std::string                 GenerateOutputFilePath();
+    void                        CreateFileIfNotExists(std::string filePath);
+    void                        WriteData(std::string filename, const std::string& data);
+    std::string                 GenerateLogLine(const std::string &message);
+    void                        RecordAppLogs(const std::string& data);
     void                        PrintMessage(const std::string& message);
     void                        PrintError(const std::string& message);
     void                        PrintSuccess(const std::string& message);
@@ -31,5 +34,7 @@ private:
     static const std::string    usageFilePath;
     static const std::string    logsFilePath;
     static const std::string    outputDirectoryPath;
+    static bool                 exitApp;
 };
+
 #endif // SERIALHANDLER_H
