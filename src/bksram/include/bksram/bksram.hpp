4dd7d14efc7adc0cb023975a62a4f04d60ad2e04
Michael Murithi <mykemitrichev@gmail.com>
2024-11-05 12:04:36 +0100

    fix: fixed BKSRAM

--- src/bksram/include/bksram/bksram.hpp
+++ src/bksram/include/bksram/bksram.hpp
@@ -3,11 +3,12 @@
 #include <cstdint>
 #include <ubitint.hpp>
 #include "stm32h7xx_hal.h"
-
 #include "util/util.hpp"
 
 namespace bksram {
     using uint20_t = ubitint_t<20>;
+
+    // Define the error codes as 20-bit unsigned integers
     struct ErrorCodes {
         static constexpr uint20_t MISSING_LF { 0xE'01'11 };
         static constexpr uint20_t HWTEST     { 0xE'10'00 };
@@ -20,28 +21,40 @@ namespace bksram {
         >;
     };
 
+    // Function template to write to BKPSRAM
     template<uint20_t value>
     inline void write() {
+        // Enable access to the backup domain
         HAL_PWR_EnableBkUpAccess();
+        // Enable the Backup SRAM regulator for retention
         HAL_PWREx_EnableBkUpReg();
-        __HAL_RCC_BKPSRAM_CLK_ENABLE();
+        // Enable the clock for BKPSRAM
+        __HAL_RCC_BKPRAM_CLK_ENABLE();
 
-        *reinterpret_cast<uint32_t*>(BKPSRAM_BASE) = ErrorCodes::Registry::get<value>().unwrap();
+        // Write the value to the Backup SRAM
+        *reinterpret_cast<uint32_t*>(D3_BKPSRAM_BASE) = ErrorCodes::Registry::get<value>().unwrap();
     }
 
+    // Function template to write to BKPSRAM and reset the system
     template<uint20_t value>
     inline void write_reset() {
         write<value>();
-        NVIC_SystemReset();
+        NVIC_SystemReset();  // Reset the system
     }
 
+    // Function to read from BKPSRAM
     inline ubitint_t<20> read() {
+        // Enable access to the backup domain
         HAL_PWR_EnableBkUpAccess();
+        // Enable the Backup SRAM regulator for retention
         HAL_PWREx_EnableBkUpReg();
-        __HAL_RCC_BKPSRAM_CLK_ENABLE();
+        // Enable the clock for BKPSRAM
+        __HAL_RCC_BKPRAM_CLK_ENABLE();
 
-        return *reinterpret_cast<uint32_t*>(BKPSRAM_BASE);
+        // Read the value from the Backup SRAM
+        return *reinterpret_cast<uint32_t*>(D3_BKPSRAM_BASE);
     }
 
+    // Function to test the Backup SRAM functionality
     bool test();
 }
